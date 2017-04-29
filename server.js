04be1c39e8caf72b641c70371714375bec5bacb4
giuseppetanzi <giuse00@gmail.com>
2017-04-29 14:53:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ console.log(certificate);
 
 var credentials = {key: privateKey, cert: certificate};
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
