78a584b24102e9424e54554749e77fdf4d1e6298
giuseppetanzi <giuse00@gmail.com>
2017-07-18 00:17:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/', function (req, res) {
   if (db) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now(), new Date().toISOString(), random: Math.random()});
+    col.insert({ip: req.ip, date: Date.now(), (new Date()).toISOString(), random: Math.random()});
     col.count(function(err, count){
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
